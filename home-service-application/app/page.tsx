d6a6d1f236d7b13bf2c55d7dbdab603e26445825
Bavyadharshini-Rajaganapathy <bavyadharshinir.22cse@kongu.edu>
2025-02-14 09:26:09 +0530

    Home Page Template Done

--- home-service-application/app/page.tsx
+++ home-service-application/app/page.tsx
@@ -1,8 +1,10 @@
-import Header from "./_components/Header"
+import Hero from "./_components/Hero";
+import Header from "./_components/Header";
+
 export default function Home() {
   return (
     <div>
-      
+      <Hero/>
     </div>
   );
 }
